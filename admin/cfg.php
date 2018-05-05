0d391da6e0c8c3d1216e8cf37c3db1def2a71fa2
Big Brother <bigbrother@bigs-mbp.home>
2018-05-05 19:35:34 +0100

    first commit

--- admin/cfg.php
+++ admin/cfg.php
@@ -1,17 +1,10 @@
 <?php
 class object {}
 $CFG = new object();
-if ($_SERVER['SERVER_NAME'] == 'localhost') {
-	$CFG->dbhost = "localhost";
-	$CFG->dbname = "bitexchange_cash";
-	$CFG->dbuser = "root";
-	$CFG->dbpass = "";
-} else {
-	$CFG->dbhost = "localhost";
-$CFG->dbname = "bitexchange_cash";
-$CFG->dbuser = "root";
-$CFG->dbpass = "xchange@123";
-}
 
+$CFG->dbhost = "localhost";
+$CFG->dbname = "square_exchange";
+$CFG->dbuser = "root";
+$CFG->dbpass = "QRft7CqUc9n6";
 
 ?>
